dbe2c7f2808031c28aada9a62a9aa14b60b90a51
jectoric <mymac@Mys-MacBook-Air.local>
2023-08-21 21:48:53 +0300

    Added UI comparison structures and updated file names

---
+++ google-translate-autotests/helpers/image-comparison.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import { PNG } from 'pngjs';
+import pixelmatch = require('pixelmatch');
+
+export const isImagesPixelsMatch = (img1Path: string, img2Path: string, path: any) => {
+    const img1 = getImageByPath(img1Path);
+    const img2 = getImageByPath(img2Path);
+    const { width, height } = img1;
+    const diff = new PNG({ width, height });
+    const numPixels = width * height;
+    const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, width, height, { threshold: 0.1 });
+    const diffPercent = `${numDiffPixels / numPixels * 100}%`;
+    fs.writeFileSync(path, PNG.sync.write(diff));
+    console.log('debug', `Pixels number of difference between images: ${numDiffPixels} pixels or ${diffPercent}`);
+    return diffPercent;
+};
+
+export const getImageByPath = (imgPath: string) => {
+    const imgData = PNG.sync.read(fs.readFileSync(imgPath));
+    return imgData;
+};
