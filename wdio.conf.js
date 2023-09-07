850596be2eda9661fcff452093d649fd2add2106
jectoric <mymac@Mys-MacBook-Air.local>
2023-09-07 23:00:14 +0300

    Fully fixed allure

--- wdio.conf.js
+++ wdio.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     },
     suites: {
         smoke_tests: ['./google-translate-autotests/tests/functional-autotests/smoke-tests/*.spec.ts'],
-        regression_tests: ['./google-translate-autotests/tests/functional-autotests/regression-tests/Regression-document-translate.spec.ts'],
+        regression_tests: ['./google-translate-autotests/tests/functional-autotests/regression-tests/*.spec.ts'],
         ui_comparison_tests: ['./google-translate-autotests/tests/non-functional-autotests/ui-comparison-tests/**/*.spec.ts']
     },
     maxInstances: 10,
@@ -73,31 +73,33 @@ exports.config = {
         }
         rmdir(downloadDir)
 
+        // Skipping this to prevent long deployment on Github Actions
+
         // Generate Allure report
-        const generation = allure(['generate', 'allure-results', '--clean']);
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(() => {
-                console.error('Error: Allure report generation timed out');
-                reject(new Error('Could not generate Allure report within the timeout'));
-            }, 20000);
+        // const generation = allure(['generate', 'allure-results', '--clean']);
+        // return new Promise((resolve, reject) => {
+        //     const generationTimeout = setTimeout(() => {
+        //         console.error('Error: Allure report generation timed out');
+        //         reject(new Error('Could not generate Allure report within the timeout'));
+        //     }, 20000);
 
-            generation.on('exit', function (exitCode) {
-                clearTimeout(generationTimeout);
+        //     generation.on('exit', function (exitCode) {
+        //         clearTimeout(generationTimeout);
 
-                if (exitCode === 0) {
-                    console.log('Allure report successfully generated');
-                    resolve();
-                } else {
-                    console.error(`Error: Allure report generation failed with exit code ${exitCode}`);
-                    reject(new Error('Could not generate Allure report'));
-                }
-            });
+        //         if (exitCode === 0) {
+        //             console.log('Allure report successfully generated');
+        //             resolve();
+        //         } else {
+        //             console.error(`Error: Allure report generation failed with exit code ${exitCode}`);
+        //             reject(new Error('Could not generate Allure report'));
+        //         }
+        //     });
 
-            // Capture any errors during report generation
-            generation.on('error', function (error) {
-                console.error('Error: An error occurred during Allure report generation:', error);
-                reject(new Error('Could not generate Allure report due to an error'));
-            });
-        });
+        //     // Capture any errors during report generation
+        //     generation.on('error', function (error) {
+        //         console.error('Error: An error occurred during Allure report generation:', error);
+        //         reject(new Error('Could not generate Allure report due to an error'));
+        //     });
+        // });
     },
 }
